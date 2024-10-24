0d9e7e80218b0bceb1ada5b1373dcdbd95e16663
Roman Ponomarev <xgerfy@gmail.com>
2024-10-25 00:07:39 +0400

    add nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import Navbar from "./components/navbar";
+
+// enum SelectedPage {
+//   Home = "home",
+//   Benefits = "benefits",
+//   OurClasses = "ourclasses",
+//   ContactUs = "contactus",
+// }
+
 const App = () => {
-  return <div>app</div>;
+const [selectedPage, setSelectedPage] = useState<string>("home")
+
+  return <div className="app bg-gray-20">
+    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+  </div>;
 };
 
 export default App;
