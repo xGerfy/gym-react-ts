57a7bed70206070998a3ebadf9e222e5ca5d1a6e
Roman Ponomarev <xgerfy@gmail.com>
2024-10-25 23:59:06 +0400

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/gym-react-ts/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
